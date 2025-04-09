995e1dfd8d8574e042947ab2c6968e9a3dba3723
GingerHub112 <stecenkoaleksandr687@gmail.com>
2025-04-09 11:22:36 -0500

    Added command attribute and popups

--- bettertk.py
+++ bettertk.py
@@ -1,5 +1,12 @@
 from tkinter import *
+from tkinter import messagebox
 from xml.etree.ElementTree import *
+
+commands = {}
+
+def register_command(name, func):
+    """Register a command to be called when the button is pressed."""
+    commands[name] = func
 
 def initWindow(title, width, height):
     Win = Tk()
@@ -16,11 +23,21 @@ def parseXML(file, root):
     widgets = {}
 
     for i in treeRoot.iter():
+        args = {
+            "bg": i.attrib.get("bg", None),
+            "fg": i.attrib.get("fg", None),
+            "font": (i.attrib.get("font", None), int(i.attrib.get("size", 12))),
+            "width": int(i.attrib.get("width", 0)),
+            "height": int(i.attrib.get("height", 0)),
+            "state": i.attrib.get("state", "normal"),
+            "text": i.attrib.get("text", ""),
+        }
+
         if i.tag == "label":
             widgets[i.attrib["id"]] = Label(root, text=i.attrib["text"], font=(i.attrib["font"], int(i.attrib["size"])))
             widgets[i.attrib["id"]].pack(pady=int(i.attrib["padding"]))
         elif i.tag == "button":
-            widgets[i.attrib["id"]] = Button(root, text=i.attrib["text"], font=(i.attrib["font"], int(i.attrib["size"])))
+            widgets[i.attrib["id"]] = Button(root, text=i.attrib["text"], font=(i.attrib["font"], int(i.attrib["size"])), command=commands.get(i.attrib["command"], None))
             widgets[i.attrib["id"]].pack(pady=int(i.attrib["padding"]))
         elif i.tag == "entry":
             widgets[i.attrib["id"]] = Entry(root, font=(i.attrib["font"], int(i.attrib["size"])))
@@ -33,3 +50,12 @@ def parseXML(file, root):
             widgets[i.attrib["id"]].pack(pady=int(i.attrib["padding"]))
 
     return widgets
+
+def info_popup(msg):
+    messagebox.showinfo("Information", msg)
+
+def error_popup(msg):
+    messagebox.showerror("Error", msg)
+
+def warning_popup(msg):
+    messagebox.showwarning("Warning", msg)
--- main.pyw
+++ main.pyw
@@ -1,6 +1,6 @@
 import bettertk
-import turtle
 
 root = bettertk.initWindow("Bettertk example", 600, 400)
+bettertk.register_command("onClick", lambda: bettertk.info_popup("This is an info message"))
 bettertk.parseXML("view.xml", root)
 root.mainloop()
